e9ba1427600e55ee8f0e2f530e05bd011be6f56d
Jeydin21 <JeyPham21@gmail.com>
2023-02-28 16:08:39 -0600

    I did some cool things

--- components/anime-details/AnimeDetails.jsx
+++ components/anime-details/AnimeDetails.jsx
@@ -33,7 +33,7 @@ function AnimeDetails({ data }) {
             onClick={() => setIsReadMore(!isReadMore)}
             className=" text-indigo-200  cursor-pointer"
           >
-            {!isReadMore ? "read more" : "read less"}
+            {!isReadMore ? "Read More" : "Read Less"}
           </span>
 
           <div className="mt-5 flex gap-2 flex-wrap">
