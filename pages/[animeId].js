1a2cd2699df9f835bc41f0caa3ab8d38ea460bb1
Jeydin21 <JeyPham21@gmail.com>
2023-03-25 11:36:40 -0500

    FIXED THE ISSUE

--- pages/[animeId].js
+++ pages/[animeId].js
@@ -11,7 +11,7 @@ export const getServerSideProps = async (context) => {
   const { animeId } = context.query;
 
   const res = await fetch(
-    `https://zubayr-theater-api.onrender.com/${animeId}`
+    `https://zubayr-theater-api.onrender.com/anime-details/${animeId}`
   );
 
   const data = await res.json();
