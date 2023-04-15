bf9907b9718798ea1ffb37aa8ac60852d33b0f5a
Jeydin21 <JeyPham21@gmail.com>
2023-04-15 12:13:58 -0500

    Added tawk.to widget

--- pages/index.js
+++ pages/index.js
@@ -2,52 +2,53 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect } from "react";
 import LandingPage from "../components/LandingPage";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
 import styles from "../styles/Home.module.css";
 
 import ReactGA from "react-ga";
 
 export default function Home() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  });
-  return (
-    <div>
-      <Head>
-        <title>Munchyroll</title>
-        <meta
-          name="title"
-          content="Munchyroll - Watch HD Anime For Free"
-        />
-        <meta
-          name="description"
-          content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
-        />
+	useEffect(() => {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	});
+	return (
+		<div>
+			<div className="App">
+				<TawkMessengerReact propertyId="622047bb1ffac05b1d7cb217" widgetId="1ft71gdb9" />
+			</div>
+			<Head>
+				<title>Munchyroll</title>
+				<meta name="title" content="Munchyroll - Watch HD Anime For Free" />
+				<meta
+					name="description"
+					content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
+				/>
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://zubayr.vercel.app/" />
-        <meta
-          property="og:title"
-          content="Munchyroll - Watch HD Anime For Free"
-        />
-        <meta
-          property="og:description"
-          content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
-        />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://zubayr.vercel.app/" />
+				<meta
+					property="og:title"
+					content="Munchyroll - Watch HD Anime For Free"
+				/>
+				<meta
+					property="og:description"
+					content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
+				/>
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://zubayr.vercel.app/" />
-        <meta
-          property="twitter:title"
-          content="Munchyroll - Watch HD Anime For Free"
-        />
-        <meta
-          property="twitter:description"
-          content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
-        />
-        <meta name="theme-color" content="#A0956E" />
-      </Head>
-      <LandingPage />
-    </div>
-  );
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://zubayr.vercel.app/" />
+				<meta
+					property="twitter:title"
+					content="Munchyroll - Watch HD Anime For Free"
+				/>
+				<meta
+					property="twitter:description"
+					content="Watch free anime series and movies online in HD quality, on mobile or on desktop, subbed or dubbed, in a rich and modern display."
+				/>
+				<meta name="theme-color" content="#A0956E" />
+			</Head>
+			<LandingPage />
+		</div>
+	);
 }
