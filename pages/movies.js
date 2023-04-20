a0efdbf743353598afb2bb696c437656090900f1
Jeydin21 <JeyPham21@gmail.com>
2023-04-19 22:24:30 -0500

    Fixed anime movies page title

--- pages/movies.js
+++ pages/movies.js
@@ -42,7 +42,7 @@ const Home = ({ data }) => {
 
       {data && (
           <>
-            <h1 className=" text-2xl font-bold">Popular Anime</h1>
+            <h1 className=" text-2xl font-bold">Anime Movies</h1>
           <div className=" mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
             {data &&
               data.map((anime) => <Card key={anime.animeId} data={anime} />)}
