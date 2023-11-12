87c7cc5a3e7d57bb31ad7573e137a72d4ae694ce
Jeydin21 <JeyPham21@gmail.com>
2023-11-11 18:36:16 -0600

    NEXT AND PREVIOUS BUTTONS

--- pages/watch/[episodeId].js
+++ pages/watch/[episodeId].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { TextButtons } from "../../components/anime-details/AnimeDetails";
 import PrimaryButton from "../../components/buttons/PrimaryButton";
@@ -7,6 +7,7 @@ import MainLayout from "../../components/layout/MainLayout";
 import VideoPlayer from "../../components/Player/VideoPlayer";
 import { HiOutlineDownload } from "react-icons/hi";
 import { BsFillPlayFill } from "react-icons/bs";
+import Link from "next/link";
 
 export const getServerSideProps = async (context) => {
   const { episodeId } = await context.query;
@@ -33,8 +34,12 @@ export const getServerSideProps = async (context) => {
 function StreamingPage({ episode, anime, episodeId }) {
   const router = useRouter();
 
-  const [isExternalPlayer, setIsExternalPlayer] = React.useState(true);
-  const [videoSource, setVideoSource] = React.useState(episode?.sources[0].file);
+  const [isExternalPlayer, setIsExternalPlayer] = useState(true);
+  const [videoSource, setVideoSource] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Add this line
+  const [triggerRender, setTriggerRender] = useState(false);
+  const [fadeout, setFadeout] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
   // const { episodeId } = router.query;
 
   // get the search id from the url with javascript
@@ -54,6 +59,19 @@ function StreamingPage({ episode, anime, episodeId }) {
   //using a blankspace as a separator
   const capitalizedEpisodeName = arr.join(" ");
 
+  const episodeNumber = parseInt(episodeId.split('-').pop());
+
+  useEffect(() => {
+    getEpisodeData(episodeNumber).then(episodeData => {
+      setVideoSource(episodeData.sources[0].file);
+      setFadeout(true);
+      setTimeout(() => setIsLoading(false), 1500); // Adjust delay to match transition duration
+      setTimeout(() => setFadeIn(true), 2000); // Adjust delay to match transition duration
+    }).catch(error => {
+      console.error(error);
+    });
+  }, [episodeId, triggerRender]);
+
   async function getEpisodeData(episodeNum) {
     const episodeData = await fetch(
       `https://munchyroll-api.onrender.com/vidcdn/watch/${episodeId.replace(/-episode-\d+/, '')}-episode-${episodeNum}`,
@@ -62,9 +80,16 @@ function StreamingPage({ episode, anime, episodeId }) {
     return episodeStuff;
   }
 
-  function changeVideoSource(source) {
-    setVideoSource(source)
-  }
+  const handleNextEpisode = () => {
+    // Your existing code...
+    setTriggerRender(prevState => !prevState);
+  };
+
+  const handlePreviousEpisode = () => {
+    // Your existing code...
+    setTriggerRender(prevState => !prevState);
+  };
+
 
   // if (!episodeId) {
   //   return <MainLayout>loading...</MainLayout>;
@@ -91,6 +116,12 @@ function StreamingPage({ episode, anime, episodeId }) {
   //     "_blank",
   //   );
   // };
+
+  if (isLoading) {
+    return (<div className={`flex items-center justify-center h-screen transition-opacity duration-1000 ${fadeout ? 'opacity-0' : 'opacity-100'}`}>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+    </div>)
+  }
   return (
     <>
       <Head>
@@ -107,77 +138,45 @@ function StreamingPage({ episode, anime, episodeId }) {
         <link rel="manifest" href="public/manifest.json" />
         {/* Maybe change this to scan image and return main color */}
       </Head>
-      <MainLayout useHead={false}>
-        {/* {isLoading && <Loading />} */}
-        {episode && (
-          <div className=" lg:flex lg:space-x-4">
-            <div className=" alignfull w-full overflow-hidden max-w-screen-xl rounded-lg">
-              {!isExternalPlayer ? (
-                <iframe
-                  className=" overflow-hidden aspect-[5/4]   sm:aspect-video w-full h-full"
-                  src={episode.Referer}
-                  allowFullScreen
-                  frameborder="0"
-                ></iframe>
-              ) : (
-                <VideoPlayer videoSource={videoSource} />
-              )}
+      <div className={`transition-opacity duration-3000 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
+        <MainLayout useHead={false}>
+          {episode && (
+            <div className=" lg:flex lg:space-x-4">
+              <div className=" alignfull w-full overflow-hidden max-w-screen-xl rounded-lg">
+                {!isExternalPlayer ? (
+                  <iframe
+                    className=" overflow-hidden aspect-[5/4]   sm:aspect-video w-full h-full"
+                    src={videoSource}
+                    allowFullScreen
+                    frameborder="0"
+                  ></iframe>
+                ) : (
+                  <VideoPlayer videoSource={videoSource} />
+                )}
 
-              <div className="font-bold hidden sm:block mt-5">
-                <h2 className="  capitalize ">{episodeName}</h2>
-              </div>
-            </div>
-            <div className="font-bold sm:hidden mt-5">
-              <h2 className=" capitalize ">{episodeName}</h2>
-
-            </div>
-
-            <div>
-              <h2 className="font-semibold lg:text-center mt-10">Episode List (Not Working)</h2>
-              <div className="lg:justify-center lg:px-20 mt-5 flex flex-wrap gap-3">
-                {anime.episodesList
-                  ?.slice(0)
-                  .reverse()
-                  .map((episode, i) => (
-                    <TextButtons
-                      key={i}
-                      // link={`/watch/${episode.episodeId}`}
-                      text={episode.episodeNum}
-                      isCurrent={episode.episodeId === episodeId}
-                      onClick={() => getEpisodeData(episode.episodeNum).then(episodeData => {
-                          changeVideoSource(episodeData.sources[0].file)
-                        }).catch(error => {
-                          console.error(error);
-                        })
-                        }
-                    />
-                  ))}
-              </div>
-            </div>
-
-            {/* <div>
-                <div className=" mt-5 lg:mt-0 space-y-4">
-                  <div className="mt-5 flex flex-wrap gap-3">
-                    {nextEpisodeId && (
-                      <TextButtons
-                        link={`/watch/${episodeId.replace(/-episode-\d+/, '')}-episode-${nextEpisodeId}`}
-                        text="Next Episode"
-                      />
-                    )}
-                  </div>
-                  <div className="mt-5 flex flex-wrap gap-3">
-                    {prevEpisodeId && (
-                      <TextButtons
-                        link={`/watch/${episodeId.replace(/-episode-\d+/, '')}-episode-${prevEpisodeId}`}
-                        text="Previous Episode"
-                      />
-                    )}
-                  </div>
-
+                <div className="flex justify-between pt-5">
+                {episodeNumber > 1 && (
+                  <Link className="justify-start" href={`/watch/${episodeId.replace(/-episode-\d+/, '')}-episode-${episodeNumber - 1}`}>
+                    <button className="bg-[#2f6b91] hover:bg-[#214861] text-white font-bold py-2 px-4 rounded" onClick={() => { handlePreviousEpisode(); setIsLoading(true); }}>Previous Episode</button>
+                  </Link>
+                )}
+                {episodeNumber < anime.episodesList.length && (
+                  <Link className="justify-end" href={`/watch/${episodeId.replace(/-episode-\d+/, '')}-episode-${episodeNumber + 1}`}>
+                    <button className="bg-[#2f6b91] hover:bg-[#214861] text-white font-bold py-2 px-4 rounded" onClick={() => { handleNextEpisode(); setIsLoading(true); }}>Next Episode</button>
+                  </Link>
+                  )}
                 </div>
-              </div> */}
 
-            {/*<PrimaryButton
+                <div className="font-bold hidden sm:block mt-5">
+                  <h2 className="  capitalize ">{episodeName}</h2>
+                </div>
+              </div>
+              <div className="font-bold sm:hidden mt-5">
+                <h2 className=" capitalize ">{episodeName}</h2>
+
+              </div>
+
+              {/*<PrimaryButton
 								icon={<BsFillPlayCircleFill />}
 								onClick={handleMxPlayer}
 								sub="Android (Experimental)">
@@ -190,28 +189,29 @@ function StreamingPage({ episode, anime, episodeId }) {
 								Open in VLC
 							</PrimaryButton>
 							*/}
-          </div>
-        )}
-        <div className="max-w-xs mt-10 space-y-4">
-          {!isExternalPlayer ? (
-            <PrimaryButton
-              icon={<BsFillPlayFill />}
-              sub="Faster And No Ads"
-              onClick={() => setIsExternalPlayer(!isExternalPlayer)}
-            >
-              Use Internal Player
-            </PrimaryButton>
-          ) : (
-            <PrimaryButton
-              icon={<HiOutlineDownload />}
-              sub="Download Option Available"
-              onClick={() => setIsExternalPlayer(!isExternalPlayer)}
-            >
-              Use External Player
-            </PrimaryButton>
+            </div>
           )}
-        </div>
-      </MainLayout>
+          <div className="max-w-xs mt-10 space-y-4">
+            {!isExternalPlayer ? (
+              <PrimaryButton
+                icon={<BsFillPlayFill />}
+                sub="Faster And No Ads"
+                onClick={() => setIsExternalPlayer(!isExternalPlayer)}
+              >
+                Use Internal Player
+              </PrimaryButton>
+            ) : (
+              <PrimaryButton
+                icon={<HiOutlineDownload />}
+                sub="Download Option Available"
+                onClick={() => setIsExternalPlayer(!isExternalPlayer)}
+              >
+                Use External Player
+              </PrimaryButton>
+            )}
+          </div>
+        </MainLayout>
+      </div>
     </>
   );
 }
