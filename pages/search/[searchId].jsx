53524b33ada62feb829ca6bb9ef9a9715c37cf21
Jeydin21 <JeyPham21@gmail.com>
2024-02-25 22:51:00 -0600

    Troll idea to hide any dubbed anime on the search page

--- pages/search/[searchId].jsx
+++ pages/search/[searchId].jsx
@@ -65,7 +65,7 @@ function SearchPage({ data }) {
         {data && (
           <>
             <div className=" ">
-              <h1 className=" text-2xl font-bold">Searched Results</h1>
+              <h1 className=" text-2xl font-bold">Search Results for: {searchId}</h1>
 
               {data.length === 0 && (
                 <div className=" mt-10 text-2xl ">No Results Found</div>
@@ -73,9 +73,11 @@ function SearchPage({ data }) {
             </div>
             <div className=" mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
               {data &&
-                data?.map((anime) => (
-                  <SearchCard key={anime.animeId} data={anime} />
-                ))}
+                data
+                  // .filter((anime) => !anime.animeTitle.toLowerCase().includes("dub"))
+                  .map((anime) => (
+                    <SearchCard key={anime.animeId} data={anime} />
+                  ))}
             </div>
           </>
         )}
