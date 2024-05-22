ccaa1367e55b4b8537622a6849c80b8ea96dec5c
Jeydin21 <jeydin@j21.dev>
2024-05-21 23:22:10 -0500

    Fix large data issue hopefully

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -3,12 +3,12 @@ const apiLink = process.env.NEXT_PUBLIC_CONSUMET_API;
 
 
 export const getAnimeDetails = async (id) => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/data/${id}`)
+  const data = await fetch(apiLink + `/meta/anilist/data/${id}`)
   return await data.json();
 };
 
 export const getAnimeEpisodeData = async (id) => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/episodes/${id}`)
+  const data = await fetch(apiLink + `/meta/anilist/episodes/${id}`)
   return await data.json();
 }
 
@@ -18,21 +18,21 @@ export const getAnimeEpisodeLinks = async (animeId) => {
 }
 
 export const getAnimeSearch = async (query) => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/search/${query}`)
+  const data = await fetch(apiLink + `/meta/anilist/search/${query}`)
   return await data.json();
 }
 
 export const getAnimeTrending = async (count) => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/trending?&perPage=${count}`)
+  const data = await fetch(apiLink + `/meta/anilist/trending?&perPage=${count}`)
   return await data.json();
 }
 
 export const getAnimePopular = async (count) => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/popular?&perPage=${count}`)
+  const data = await fetch(apiLink + `/meta/anilist/popular?&perPage=${count}`)
   return await data.json();
 }
 
 export const getAnimeNew = async () => {
-  const data = await fetch(corsLink + '/' + apiLink + `/meta/anilist/recent-episodes`)
+  const data = await fetch(apiLink + `/meta/anilist/recent-episodes`)
   return await data.json();
 }
