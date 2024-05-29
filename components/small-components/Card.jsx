1c139415041a7081efe47699fb07c61a07ad1feb
Jeydin21 <JeyPham21@gmail.com>
2024-05-28 23:07:47 -0500

    Fixed some title error (look into this)

--- components/small-components/Card.jsx
+++ components/small-components/Card.jsx
@@ -26,52 +26,56 @@ function Card({ data }) {
   if (!seriesData) return null; // or a loading spinner
 
   return (
-    <Link href={"/anime/" + seriesData.id}>
-  <div className="group sm:p-3">
-    <div className="overflow-hidden rounded-lg relative">
-      <div className="transition-all transform duration-300 group-hover:scale-105 group-hover:brightness-50">
-        <LazyLoadImage
-          effect="blur"
-          className="w-full aspect-[5/7] object-cover rounded-lg"
-          src={seriesData.image}
-          alt=""
-        />
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <FaPlay className="text-4xl" />
-      </div>
-    </div>
+    <>
+      {seriesData.title && (
+        <Link href={"/anime/" + seriesData.id}>
+          <div className="group sm:p-3">
+            <div className="overflow-hidden rounded-lg relative">
+              <div className="transition-all transform duration-300 group-hover:scale-105 group-hover:brightness-50">
+                <LazyLoadImage
+                  effect="blur"
+                  className="w-full aspect-[5/7] object-cover rounded-lg"
+                  src={seriesData.image}
+                  alt=""
+                />
+              </div>
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <FaPlay className="text-4xl" />
+              </div>
+            </div>
 
-    <div className="dark:text-secondary text-primary">
-      <h4 className="mt-3 font-bold line-clamp-2">{seriesData.title.english || seriesData.title.romaji}</h4>
+            <div className="dark:text-secondary text-primary">
+              <h4 className="mt-3 font-bold line-clamp-2">{seriesData.title.english || seriesData.title.romaji}</h4>
 
-      {/* <p>Episode {seriesData.episodeNumber}</p> */}
-          <div className="flex space-x-2 mt-2 select-none">
-            <p className="dark:text-secondary text-primary">{seriesData.type}
-            {seriesData.releaseDate && (
-                  <span>•{seriesData.releaseDate}</span>
-              )}
-            </p>
-            {seriesData.totalEpisodes && seriesData.totalEpisodes != 1 && (
-              <p className="max-xl:hidden">
-                <span className="dark:text-secondary text-primary flex items-center">
-                  <FaBook />
-                  <span className="ml-1">{seriesData.totalEpisodes}</span>
-                </span>
-              </p>
-            )}
-            {seriesData.rating && (
-              <p>
-                <span className="dark:text-secondary text-primary flex items-center">
-                  <FaStar />
-                  <span className="ml-1">{seriesData.rating}</span>
-                </span>
-              </p>
-            )}
+              {/* <p>Episode {seriesData.episodeNumber}</p> */}
+              <div className="flex space-x-2 mt-2 select-none">
+                <p className="dark:text-secondary text-primary">{seriesData.type}
+                  {seriesData.releaseDate && (
+                    <span>•{seriesData.releaseDate}</span>
+                  )}
+                </p>
+                {seriesData.totalEpisodes && seriesData.totalEpisodes != 1 && (
+                  <p className="max-xl:hidden">
+                    <span className="dark:text-secondary text-primary flex items-center">
+                      <FaBook />
+                      <span className="ml-1">{seriesData.totalEpisodes}</span>
+                    </span>
+                  </p>
+                )}
+                {seriesData.rating && (
+                  <p>
+                    <span className="dark:text-secondary text-primary flex items-center">
+                      <FaStar />
+                      <span className="ml-1">{seriesData.rating}</span>
+                    </span>
+                  </p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      )}
+    </>
   );
 }
 
