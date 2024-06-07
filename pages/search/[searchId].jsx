ae0763ac97b7e27110b224d0e41aa17ae37f779b
Jeydin21 <JeyPham21@gmail.com>
2024-06-07 00:25:46 -0500

    Optimized anime cards size on home and search page

--- pages/search/[searchId].jsx
+++ pages/search/[searchId].jsx
@@ -52,7 +52,7 @@ function SearchPage({ data }) {
                 <div className=" mt-10 text-2xl ">No Results Found</div>
               )}
             </div>
-            <div className=" mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
+            <div className=" mt-5 grid grid-cols-3 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 auto-rows-[1fr] 2xl:grid-cols-7">
               {data &&
                 data.results
                   // .filter((anime) => !anime.animeTitle.toLowerCase().includes("dub"))
