a6e33f546073b765f0e19ba019cd968366fa5545
Jeydin21 <JeyPham21@gmail.com>
2024-06-10 01:26:53 -0500

    Real anime card hover effect fix

--- components/small-components/Card.jsx
+++ components/small-components/Card.jsx
@@ -45,22 +45,24 @@ function Card({ data, release }) {
     <>
       <Link href={"/anime/" + data.id}>
         <div className="group sm:p-3 ">
-          <div className="overflow-hidden relative rounded-lg">
-            <div className="transition-all transform duration-300 group-hover:scale-105 group-hover:brightness-50">
-              <LazyLoadImage
-                effect="blur"
-                className="w-full h-full aspect-[5/7] object-cover rounded-lg"
-                src={data.image}
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <FaPlay className="text-4xl" />
+          <div className="overflow-hidden relative rounded-lg aspect-[5/7]">
+            <div className="absolute inset-0">
+              <div className="transition-all transform duration-300 group-hover:scale-105 group-hover:brightness-50">
+                <LazyLoadImage
+                  effect="blur"
+                  className="w-full h-full aspect-[5/7] object-cover rounded-lg"
+                  src={data.image}
+                  alt=""
+                />
+              </div>
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <FaPlay className="text-4xl" />
+              </div>
             </div>
           </div>
 
           <div className="dark:text-secondary text-primary">
-            <h4 className="mt-3 font-bold line-clamp-2">{data.title.english || data.title.romaji}</h4>
+            <h4 className="mt-2 font-bold line-clamp-2">{data.title.english || data.title.romaji}</h4>
 
             {/* <p>Episode {data.episodeNumber}</p> */}
             <div className="flex space-x-2 mt-2 select-none">
