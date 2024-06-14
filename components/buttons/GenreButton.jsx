82159bd4d8ad0bde259f94cb7878e22f1f628074
Jeydin21 <JeyPham21@gmail.com>
2024-06-14 12:54:00 -0500

    chore: Updated genre buttons on manga pages

--- components/buttons/GenreButton.jsx
+++ components/buttons/GenreButton.jsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 
-function GenreButton({ text }) {
+function GenreButton({ text, link = true }) {
   return (
     <div className={`bg-primary-light border-gray-300 hover:bg-slate-700 rounded-full text-center min-w-[75px] transition-all py-1 px-3`}>
-      <Link href={`/anime/genre/${text.toLowerCase()}`}>
+      {link ? (
+        <Link href={`/anime/genre/${text.toLowerCase()}`}>
+          <p className="text-secondary">{text}</p>
+        </Link>
+      ) : (
         <p className="text-secondary">{text}</p>
-      </Link>
+      )}
     </div>
   );
 };
