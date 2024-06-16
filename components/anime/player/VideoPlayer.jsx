7c95875556ee674ec02f3edfa5a29ce89c147b51
Jeydin21 <JeyPham21@gmail.com>
2024-06-16 03:38:39 -0500

    chore: Videojs city theme

--- components/anime/player/VideoPlayer.jsx
+++ components/anime/player/VideoPlayer.jsx
@@ -3,6 +3,7 @@ import videojs from "video.js";
 import "@videojs/http-streaming";
 import "video.js/dist/video-js.css";
 import "videojs-hotkeys";
+import '@videojs/themes/dist/city/index.css';
 
 const corsLink = process.env.NEXT_PUBLIC_CORS_REQUEST_LINK
 
@@ -81,7 +82,7 @@ const VideoPlayer = ({ videoSource }) => {
           id="videoPlayerHLS"
           onContextMenu={(e) => e.preventDefault()}
           ref={videoRef}
-          className="video-js vjs-big-play-centered"
+          className="video-js vjs-theme-city vjs-big-play-centered"
         ></video>
       </div>
     </div>
