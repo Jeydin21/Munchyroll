006e6798b4af55385ae76eb8e50c1c45e129eaa0
Jeydin21 <JeyPham21@gmail.com>
2024-07-13 22:01:15 -0500

    feat: Added quality selector

--- components/anime/player/VideoPlayer.jsx
+++ components/anime/player/VideoPlayer.jsx
@@ -1,15 +1,13 @@
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getAnimeEpisodeLinks } from "../../../src/handlers/anime";
 import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
-import { MediaPlayer, MediaProvider } from '@vidstack/react';
-import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react';
+import { defaultLayoutIcons, DefaultVideoLayout, DefaultAudioLayout } from '@vidstack/react/player/layouts/default';
 
 const corsLink = process.env.NEXT_PUBLIC_CORS_REQUEST_LINK
 
 const VideoPlayer = ({ episodeTitle, episodeName, episodeThumbnail }) => {
-  const videoRef = useRef();
-
   const [episodeDataLink, setEpisodeDataLink] = useState(null);
 
   useEffect(() => {
@@ -17,7 +15,8 @@ const VideoPlayer = ({ episodeTitle, episodeName, episodeThumbnail }) => {
       try {
         const episodeData = await getAnimeEpisodeLinks(episodeName);
         if (episodeData && episodeData.sources && episodeData.sources[3] && episodeData.sources[3].url) {
-          setEpisodeDataLink(episodeData.sources[3].url);
+          const defaultSource = episodeData.sources.find(source => source.quality === "default");
+          setEpisodeDataLink(defaultSource.url);
         } else {
           console.error("Episode data is missing or the expected structure is not met.");
           setEpisodeDataLink(null); // Explicitly setting to null if condition fails
@@ -27,19 +26,22 @@ const VideoPlayer = ({ episodeTitle, episodeName, episodeThumbnail }) => {
         setEpisodeDataLink(null); // Consider setting to a fallback URL or error state
       }
     };
-  
+
     fetchEpisodeData();
   }, [episodeName]); // Ensure episodeName is correctly triggering the effect
-  
+
   // Conditional rendering or providing a default src
   if (!episodeDataLink) {
     return <div>Loading...</div>; // Or any other fallback UI
   }
-  
+
   return (
-    <MediaPlayer title={episodeTitle} src={episodeDataLink}>
-      <MediaProvider />
-      <DefaultVideoLayout thumbnails={episodeThumbnail} icons={defaultLayoutIcons} />
+    <MediaPlayer title={episodeTitle} src={episodeDataLink} playsInline aspectRatio="16/9" load="eager" posterLoad="eager" streamType="on-demand" >
+      <MediaProvider>
+        <Poster src={episodeThumbnail} alt='' />
+      </MediaProvider>
+      <DefaultAudioLayout icons={defaultLayoutIcons} />
+      <DefaultVideoLayout icons={defaultLayoutIcons} />
     </MediaPlayer>
   );
 };
