2a1a988e5ce9454a2a00276a6c923989a9b18f79
Jeydin21 <jeydin@j21.dev>
2024-08-18 23:03:47 -0500

    chore: Added cors request link to manga reader

--- components/manga/reader/MangaReader.jsx
+++ components/manga/reader/MangaReader.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 
 function MangaReader({ pages }) {
+  const corsLink = process.env.NEXT_PUBLIC_CORS_REQUEST_LINK;
+
   return (
     <div className="items-center justify-center flex flex-col">
         {pages &&
           pages.map((item, index) => (
             <div key={index}>
               <Image
-                src={item.img}
+                src={`${corsLink}/${item.img}`}
                 key={index}
                 alt={`Page ${index + 1}`}
                 width={600}
